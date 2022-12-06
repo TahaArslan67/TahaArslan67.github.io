60a7ce8208d64607eb1a683f5cddfbc1badd930a
TahaArslan67 <arslantaha6767@gmail.com>
2022-12-07 00:25:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(password1);
 
 // ALTTAKİNİ KOPYALAYARAK HESAP EKLE
 if(username1=="taharslan" && password1=="00228t"){
-        window.location.href ="http://127.0.0.1:5501/Home.html" ;
+        window.location.href ="Home.html" ;
 }  
 
     
@@ -14,7 +14,7 @@ if(username1=="taharslan" && password1=="00228t"){
 // ÜSTTEKİNİ KOPYALAYARAK HESAP EKLE
 
 else if(username1=="taharslan2" && password1=="00228t2"){
-    window.location.href ="http://127.0.0.1:5501/Home.html" ;
+    window.location.href ="Home.html" ;
 }
 
 
