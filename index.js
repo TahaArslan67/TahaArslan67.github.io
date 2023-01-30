eeb6300c28d816ce70881ab1cd725d01679b8964
TahaArslan67 <arslantaha6767@gmail.com>
2023-01-31 01:41:17 +0300

    asdas

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ console.log(password1);
 
 // ALTTAKİNİ KOPYALAYARAK HESAP EKLE
 if(username1=="taharslan" && password1=="00228t"){
-        window.location.href ="Home.php" ;
+        window.location.href ="Home.html" ;
 }  
 else if(username1=="tunat" && password1=="dayımınözoğlu67"){
-  window.location.href ="Home.php" ;
+  window.location.href ="Home.html" ;
 } 
 else if(username1=="ayı" && password1=="şalterci"){
-  window.location.href ="Home.php" ;
+  window.location.href ="Home.html" ;
 }
     
 
 // ÜSTTEKİNİ KOPYALAYARAK HESAP EKLE
 
 else if(username1=="Taharslan" && password1=="00228t"){
-    window.location.href ="Home.php" ;
+    window.location.href ="Home.html" ;
 }
 else if(username1=="Taharslan" && password1=="0022800228t"){
-  window.location.href ="Home.php" ;
+  window.location.href ="Home.html" ;
 }
 else if(username1=="taharslan" && password1=="0022800228t"){
-  window.location.href ="Home.php" ;
+  window.location.href ="Home.html" ;
 }
 
 
